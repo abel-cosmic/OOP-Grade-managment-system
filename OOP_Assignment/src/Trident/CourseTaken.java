0ea4eccead7fb5cf114cc2abfb072a4eb0246aa3
Dagim Tesfaye Mebrate <dagimcodes@gmail.com>
2022-12-25 13:32:14 +0300

    Lots of documentation and additions in display add and delete.

--- OOP_Assignment/src/Trident/CourseTaken.java
+++ OOP_Assignment/src/Trident/CourseTaken.java
@@ -2,7 +2,12 @@ package Trident;
 
 public class CourseTaken extends Course{
     String GradeLetter;
-    public CourseTaken(String CID, int CreditHr, int ContactHr, int LabHr, int LectureHr, String CName, int CDID) {
+    public CourseTaken(String CID, int CreditHr, int ContactHr, int LabHr, int LectureHr, String CName, int CDID, String gL) {
         super(CID, CreditHr, ContactHr, LabHr, LectureHr, CName, CDID);
+        GradeLetter = "F";
+    }
+    public CourseTaken(Course C){
+        super(C.CID, C.CreditHr, C.ContactHr, C.LabHr, C.LectureHr, C.CName, C.DID);
+        GradeLetter = "F";
     }
 }
